f398402d61703ee0690b8a24a631d3541077fd45
Tancredi Covioli <tancredi.covioli@gmail.com>
2021-02-22 11:25:20 +0100

    Made the zsh configuration dependant on oh-my-zsh
    This means the deletion of all the config files. The aliases and other
    important settings were moved in the .oh-my-zsh/custom folder.

---
+++ zsh/.oh-my-zsh/custom/env.zsh
@@ -0,0 +1,2 @@
+export MAG2="$HOME/Documenti/University/Magistrale/Secondo_Anno/Secondo_Semestre"
+export MAG1="$HOME/Documenti/University/Magistrale/Secondo_Anno/Primo_Semestre"
--- zsh/.zprofile
+++
@@ -1,3 +0,0 @@
-export PATH="$PATH:$HOME/.local/bin"
-export XDG_CONFIG_HOME="$HOME/.config"
-tdm --silent
