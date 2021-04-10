a7ef613e149f9d2b6451e6f509e8c6db9c5dd15a
Tancredi Covioli <tancredi.covioli@gmail.com>
2021-04-10 14:08:54 +0200

    Addition of a directory to the path.
    The emacs.d/bin directory contains the doom binary when doom is
    installed.
    This binary is useful to mantain Doom Emacs

--- zsh/.oh-my-zsh/custom/env.zsh
+++ zsh/.oh-my-zsh/custom/env.zsh
@@ -1,2 +1,3 @@
 export MAG2="$HOME/Documenti/University/Magistrale/Secondo_Anno/Secondo_Semestre"
 export MAG1="$HOME/Documenti/University/Magistrale/Secondo_Anno/Primo_Semestre"
+export PATH="$PATH:$HOME/.emacs.d/bin"
