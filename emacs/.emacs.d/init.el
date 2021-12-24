30b3ff6cc5815a1fbafb943e2e167d1290b6d77e
Tancredi Covioli <tancredi.covioli@gmail.com>
2021-12-24 14:40:59 +0100

    [emacs] added another personal configuration other than doom
    Added a submodule to the doom directory pointing to the doom github page to be able to switch between the two configurations seamlessly

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,9 @@
+(setq visible-bell nil
+      ring-bell-function 'double-flash-mode-line)
+
+(defun double-flash-mode-line ()
+  (let ((flash-sec (/ 1.0 20)))
+    (invert-face 'mode-line)
+    (run-with-timer flash-sec nil #'invert-face 'mode-line)
+    (run-with-timer (* 2 flash-sec) nil #'invert-face 'mode-line)
+    (run-with-timer (* 3 flash-sec) nil #'invert-face 'mode-line)))
