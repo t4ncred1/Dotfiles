60b65b4652bb52cb8a5f240b0f679dea8c9f33d2
Tancredi Covioli <tancredi.covioli@liferay.com>
2026-01-21 23:43:59 +0100

    Update all configurations
    in line with new doom version and new needs

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -1,28 +1,33 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-
-
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Tancredi Covioli"
       user-mail-address "tancredi.covioli@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -35,17 +40,29 @@
       (setq doom-theme 'doom-nord-light)
     (setq doom-theme 'doom-one)))
 
-
-;; if you use `org' and don't want your org files in the default location below,q
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 't)
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Workspace/org")
 
 
-;; Here are some additional functions/macros that could help you configure Doom:
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
@@ -58,66 +75,22 @@
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+;;
 ;; Add a mapping to insert a note when org noter is active and pdf-view is enabled
 ;; in a buffer.
-(map! (:when (featurep! :lang org +noter)
+(map! (:when (modulep! :lang org +noter)
        :map pdf-view-mode-map
        :after org-noter
        :desc "Insert a note in org noter"
        :nvi "i" #'org-noter-insert-note))
 
-;; mappings for calc
-(map! :leader
-      (:prefix-map ("=" . "calc")
-       "=" #'calc-dispatch
-       "c" #'calc
-       "q" #'quick-calc
-       "g" #'calc-grab-region))
-
-;; suggested config from :lang cc
-(after! ccls
-  (setq ccls-initialization-options '(:index (:comments 0) :completion (:detailedLabel t)))
-  (set-lsp-priority! 'ccls 2)) ; optional as ccls is the default in Doom
-
-;; haskell mode configuration
-(after! haskell-mode
-;; don't open new popup windows when showing haskell errors
- (setq haskell-interactive-popup-errors nil))
-
-;;flycheck configuration
-(after! flycheck
-;; only check buffers with flycheck when the file is first opened or saved.
- (setq flycheck-check-syntax-automatically '(mode-enabled save)))
-
-;; preview buffers
-(setq +ivy-buffer-preview t)
-
-;; if the battery is available, show it
-;; (unless (string-match-p "^Battery status not available" (battery))
-;;   (display-battery-mode 1))
-
-;; display the time
-(setq display-time-24hr-format 1)
-(setq display-time-default-load-average nil)
-(display-time-mode)
-(setq display-time-interval 15)
-
-;; customizations to be used on specific devices
-(let ((distro (with-temp-buffer
-  (insert-file-contents "/etc/issue")
-  (nth 0 (split-string (buffer-string) nil t))
-  )))
-  ;; Only apply if the distribution is Debian
-  (if (string-match-p "Debian" distro)
-      (progn
-       ;; change the default dictionary to english
-       (setq debian-ispell-dictionary "english")
-       (setq ispell-dictionary "english"))))
-
+;;
 ;; Capture templates
 ;;
 (after! org
@@ -148,12 +121,28 @@
          "* %U\n %i\n %a" :heading "Journal" :prepend t)
        )))
 
-(defconst t4n/bib-bib "~/Documenti/University/Magistrale/tesi/references.bib")
-(defconst t4n/bib-library "~/Documenti/University/Magistrale/tesi/libreria")
-(defconst t4n/bib-notes "~/Documenti/University/Magistrale/tesi/note")
+;; reduce the size of the icons
+(setq all-the-icons-scale-factor 1.1)
 
-(setq bibtex-completion-bibliography t4n/bib-bib ; My bibliography PDF location
-      bibtex-completion-library-path t4n/bib-library ; My PDF lib location
+;; Disable persistent undo history
+(remove-hook 'undo-fu-mode-hook #'global-undo-fu-session-mode)
+
+;; when poetry is enabled, set a shortcut
+(map! :map python-mode-map
+      :when (featurep! :lang python +poetry)
+      :localleader
+      :desc "poetry" "p" #'poetry)
+
+;;
+;; Uni stuff, now obsolete but still viable
+;;
+
+(defconst t4n/bib-bib "~/documenti/university/magistrale/tesi/references.bib")
+(defconst t4n/bib-library "~/documenti/university/magistrale/tesi/libreria")
+(defconst t4n/bib-notes "~/documenti/university/magistrale/tesi/note")
+
+(setq bibtex-completion-bibliography t4n/bib-bib ; my bibliography pdf location
+      bibtex-completion-library-path t4n/bib-library ; my pdf lib location
       bibtex-completion-notes-path t4n/bib-notes)
 
 (after! org-noter
@@ -174,42 +163,29 @@
         citar-notes-paths (list t4n/bib-notes))
   )
 
-(after! org-roam
-  (setq org-roam-v2-ack t
-        +org-roam-open-buffer-on-find-file nil
-        org-roam-completion-everywhere nil
-        org-roam-directory t4n/bib-notes))
+;; (after! org-roam
+;;   (setq org-roam-v2-ack t
+;;         +org-roam-open-buffer-on-find-file nil
+;;         org-roam-completion-everywhere nil
+;;         org-roam-directory t4n/bib-notes))
 
-(use-package! org-roam-bibtex
-  :after org-roam
-  :config
-  (setq orb-preformat-keywords
-      '("citekey" "title" "url" "author-or-editor" "keywords" "file")
-      orb-process-file-keyword t
-      orb-attached-file-extensions '("pdf")
-      orb-roam-ref-format 'org-cite)
-  (setq org-roam-capture-templates
-        '(;; bibliography note template
-          ("r" "bibliography reference" plain "%?"
-           :if-new (file+head "${citekey}.org"
-                              "#+title: Notes on ${title}\n* ${title}\n :PROPERTIES:\n  :Custom_ID: ${citekey}\n  :URL: ${url}\n  :AUTHOR: ${author-or-editor}\n  :NOTER_DOCUMENT: %(orb-get-attached-file \"${citekey}\")\n  :NOTER_PAGE: \n  :END:\n\n")
-           :unnarrowed t)
-          ))
-  (require 'org-roam-bibtex)
-  (setq
-        citar-open-note-function 'orb-citar-edit-note
-        orb-preformat-keywords '("citekey" "title" "url" "author-or-editor" "keywords" "file")
-        orb-process-file-keyword t
-        orb-attached-file-extensions '("pdf")))
-
-;; reduce the size of the icons
-(setq all-the-icons-scale-factor 1.1)
-
-;; Disable persistent undo history
-(remove-hook 'undo-fu-mode-hook #'global-undo-fu-session-mode)
-
-;; when poetry is enabled, set a shortcut
-(map! :map python-mode-map
-      :when (featurep! :lang python +poetry)
-      :localleader
-      :desc "poetry" "p" #'poetry)
+;; (use-package! org-roam-bibtex :after org-roam
+;;   :config
+;;   (setq orb-preformat-keywords
+;;       '("citekey" "title" "url" "author-or-editor" "keywords" "file")
+;;       orb-process-file-keyword t
+;;       orb-attached-file-extensions '("pdf")
+;;       orb-roam-ref-format 'org-cite)
+;;   (setq org-roam-capture-templates
+;;         '(;; bibliography note template
+;;           ("r" "bibliography reference" plain "%?"
+;;            :if-new (file+head "${citekey}.org"
+;;                               "#+title: Notes on ${title}\n* ${title}\n :PROPERTIES:\n  :Custom_ID: ${citekey}\n  :URL: ${url}\n  :AUTHOR: ${author-or-editor}\n  :NOTER_DOCUMENT: %(orb-get-attached-file \"${citekey}\")\n  :NOTER_PAGE: \n  :END:\n\n")
+;;            :unnarrowed t)
+;;           ))
+;;   (require 'org-roam-bibtex)
+;;   (setq
+;;         citar-open-note-function 'orb-citar-edit-note
+;;         orb-preformat-keywords '("citekey" "title" "url" "author-or-editor" "keywords" "file")
+;;         orb-process-file-keyword t
+;;         orb-attached-file-extensions '("pdf")))
--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -4,8 +4,8 @@
 ;; in. Remember to run 'doom sync' after modifying it!
 
 ;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
-;;      documentation. There you'll find a "Module Index" link where you'll find
-;;      a comprehensive list of Doom's modules and what flags they support.
+;;      documentation. There you'll find a link to Doom's Module Index where all
+;;      of our modules are listed, including what flags they support.
 
 ;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
 ;;      'C-c c k' for non-vim users) to view its documentation. This works on
@@ -15,13 +15,14 @@
 ;;      directory (for easy access to its source code).
 
 (doom! :input
+       ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
        ;;chinese
        ;;japanese
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       (company            ; the ultimate code completion backend
-        +childframe)
+       ;;company           ; the ultimate code completion backend
+       (corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
@@ -31,10 +32,9 @@
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;;hydra
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
@@ -42,13 +42,12 @@
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
-       (popup              ; tame sudden yet inevitable temporary windows
-        +defaults
-        +all)
+       (popup +defaults)   ; tame sudden yet inevitable temporary windows
+       ;;smooth-scroll     ; So smooth you won't believe it's not butter
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
-       vc-gutter         ; vcs diff in the fringe
+       (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
@@ -57,22 +56,24 @@
        :editor
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
-       ;;fold              ; (nigh) universal code folding
-       (format           ; automated prettiness
-        +onsave)
+       fold              ; (nigh) universal code folding
+       ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       multiple-cursors  ; editing in many places at once
+       ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
-       word-wrap         ; soft wrapping with language-aware indent
+       (whitespace +guess +trim)  ; a butler for your whitespace
+       ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
        dired             ; making dired pretty [functional]
-       ;;electric        ; smarter, keyword-based electric-indent
-       ibuffer         ; interactive buffer management
+       electric          ; smarter, keyword-based electric-indent
+       ;;eww               ; the internet is gross
+       ;;ibuffer           ; interactive buffer management
+       tramp             ; remote files at your arthritic fingertips
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
@@ -84,40 +85,40 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +flyspell) ; tasing you for misspelling mispelling
+       ;;(spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
+       ;;biblio            ; Writes a PhD for you (citation needed)
+       ;;collab            ; buffers with friends
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       lsp               ; M-x vscode
+       ;;llm               ; when I said you needed friends, I didn't mean...
+       ;;(lsp +eglot)      ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       pdf               ; pdf enhancements
-       ;;prodigy           ; FIXME managing external services & code builders
-       rgb               ; creating color strings
-       ;;taskrunner        ; taskrunner for all your projects
+       ;;pdf               ; pdf enhancements
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
+       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
-       biblio
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
-       tty               ; improve the terminal Emacs experience
+       (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
+       ;;tty               ; improve the terminal Emacs experience
 
        :lang
+       ;;ada               ; In strong typing we (blindly) trust
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       ;;cc                ; C > C++ == 1
+       ;;(cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -133,15 +134,18 @@
        ;;ess               ; emacs speaks statistics
        ;;factor
        ;;faust             ; dsp, but you get to keep your soul
+       ;;fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
        ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
+       ;;(graphql +lsp)    ; Give queries a REST
        ;;(haskell +lsp)    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        ;;json              ; At least it ain't XML
-       (java +meghanada) ; the poster child for carpal tunnel syndrome
+       ;;janet             ; Fun fact: Janet is me!
+       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
@@ -153,22 +157,19 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org                ; organize your plain life in plain text
-        +noter
-        +roam2)
+       (org +roam)         ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
+       ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
-       (python             ; beautiful is better than ugly
-        +lsp
-        +poetry)
+       ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
@@ -191,7 +192,6 @@
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
-       ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
        ;;literate
